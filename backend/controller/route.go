8a6fa981b336d433ed882f2de53230a3671934e4
Bhuvana102 <bhuvanav102@gmail.com>
2022-02-04 12:33:29 -0500

    Refactoring the code to fetch API from the controller

--- backend/controller/route.go
+++ backend/controller/route.go
@@ -1,9 +1,39 @@
 package controller
 
-import "github.com/gorilla/mux"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"good-reads/model"
+)
 
 func Starter() *mux.Router {
 	r := mux.NewRouter()
 	subRoute := r.PathPrefix("/api").Subrouter()
 	return subRoute
+}
+
+func ApiHandler(r *mux.Router) {
+
+	r.HandleFunc("/getGenres/{id}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Request received")
+
+		vars := mux.Vars(r)
+		key := vars["id"]
+
+		body := model.FetchGenre(key)
+
+		w.Write(body)
+
+	})
+	r.HandleFunc("/getGenres", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Request received")
+
+		body := model.FetchAllGenre()
+		w.Header().Set("content-type", "application/json")
+		w.Write(body)
+
+	})
 }
--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"good-reads/controller"
 	"good-reads/model"
 
 	"github.com/gorilla/mux"
@@ -14,33 +15,9 @@ func main() {
 	model.Connect()
 	fmt.Println("Connected to database")
 	r := mux.NewRouter()
-	r.HandleFunc("/getGenres/{id}", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Request received")
 
-		vars := mux.Vars(r)
-		key := vars["id"]
+	controller.ApiHandler(r)
 
-		body := model.FetchGenre(key)
-
-		/* if r.Method == http.MethodGet {
-			data := Genres{
-				Code: http.StatusOK,
-				Body: "pong",
-			}
-			json.NewEncoder(w).Encode(data)
-
-		} */
-
-		w.Write(body)
-
-	})
-	r.HandleFunc("/getGenres", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Request received")
-
-		body := model.FetchAllGenre()
-		w.Header().Set("content-type", "application/json")
-		w.Write(body)
-
-	})
-	http.ListenAndServe("localhost:3000", r)
+	fmt.Println("Server started")
+	http.ListenAndServe("localhost:8080", r)
 }
