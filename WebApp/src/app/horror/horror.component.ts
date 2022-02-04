2eea27f4cdf3ae878dcddf3904a388228cff1504
vaishaliunnikrishnan <vaishaliunnikrishnan@gmail.com>
2022-02-05 00:00:16 +0530

    Horror page developing section 1

--- WebApp/src/app/horror/horror.component.ts
+++ WebApp/src/app/horror/horror.component.ts
@@ -15,7 +15,14 @@ export class HorrorComponent implements OnInit {
     { name: "book2", img: "book2.jpg", desc: "hi2" },
     { name: "book3", img: "book3.jpg", desc: "hi3" },
     { name: "book4", img: "book4.jpg", desc: "hi4" },
-    { name: "book5", img: "book5.jpg", desc: "hi5" }
+    { name: "book5", img: "book5.jpg", desc: "hi5" },
+    { name: "book6", img: "book6.jpg", desc: "hi6" },
+    { name: "book7", img: "book7.jpg", desc: "hi7" },
+    { name: "book8", img: "book8.jpg", desc: "hi8" },
+    { name: "book9", img: "book9.jpg", desc: "hi9" },
+    { name: "book10", img: "book10.jpg", desc: "hi10" },
+    { name: "book11", img: "book11.jpg", desc: "hi11" },
+    { name: "book12", img: "book12.jpg", desc: "hi12" }
   ]
   description = "";
   selectedbook = "";
