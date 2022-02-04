87c610013219d27b4223439db074042b3306f72f
Bhuvana102 <bhuvanav102@gmail.com>
2022-02-04 17:40:20 -0500

    Update route.go

--- backend/controller/route.go
+++ backend/controller/route.go
@@ -7,32 +7,11 @@ import (
 func Starter() *mux.Router {
 	r := mux.NewRouter()
 	subRoute := r.PathPrefix("/api").Subrouter()
+
 	subRoute.HandleFunc("/insertBook", InsertBookHandler())
+
 	subRoute.HandleFunc("/getGenres/{id}", GetGenreForIDHandler())
 	subRoute.HandleFunc("/getGenres", GetGenresHandler())
+
 	return subRoute
 }
-
-// func ApiHandler(r *mux.Router) {
-
-// 	r.HandleFunc("/getGenres/{id}", func(w http.ResponseWriter, r *http.Request) {
-// 		fmt.Println("Request received")
-
-// 		vars := mux.Vars(r)
-// 		key := vars["id"]
-
-// 		body := model.FetchGenre(key)
-
-// 		w.Header().Set("content-type", "application/json")
-// 		w.Write(body)
-
-// 	})
-// 	r.HandleFunc("/getGenres", func(w http.ResponseWriter, r *http.Request) {
-// 		fmt.Println("Request received")
-
-// 		body := model.FetchAllGenre()
-// 		w.Header().Set("content-type", "application/json")
-// 		w.Write(body)
-
-// 	})
-// }
