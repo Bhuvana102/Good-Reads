c2fc46a1b0f3e1b564a62d775743ca6ca44856ec
Srikar Chowdary Kantamani <srikar.kantamani@gmail.com>
2022-02-04 01:01:34 -0500

    Creating a request router and dispatcher using gorilla mux and starting server on port 8080

--- backend/controller/route.go
+++ backend/controller/route.go
@@ -1,1 +1,8 @@
 package controller
+
+import "github.com/gorilla/mux"
+
+func Starter() *mux.Router {
+	r := mux.NewRouter()
+	return r
+}
--- backend/main.go
+++ backend/main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"good-reads/controller"
 	"good-reads/model"
+	"log"
+	"net/http"
 )
 
 func main() {
 
+	r := controller.Starter()
 	model.Connect()
 	fmt.Println("Connected to database")
 	model.FetchGenres()
+	fmt.Println("Server started")
+	log.Fatal(http.ListenAndServe("localhost:8080", r)) //starting a server on port 8080
 	// defer db.Close
 
 }
