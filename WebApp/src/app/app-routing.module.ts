c87d69d4bc31ac6859f1f1ecb3b90e9d0580fe84
Manish Kumar Gangula <41356950+Manish0490@users.noreply.github.com>
2022-02-04 13:04:55 -0500

    Routing enabled and Browse dropdown

--- WebApp/src/app/app-routing.module.ts
+++ WebApp/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomepageComponent } from './homepage/homepage.component';
+import { CommunityComponent } from './community/community.component';
+import { CommonMenuComponent } from './common-menu/common-menu.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo:'/login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
-  { path: 'home', component: HomepageComponent},
+  { path: 'home', component: CommonMenuComponent},
+  { path: 'community', component: CommunityComponent},
 ];
 
 @NgModule({
--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { CommonMenuComponent } from './common-menu/common-menu.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
@@ -16,13 +16,22 @@ import {MatIconModule} from '@angular/material/icon'
 import {MatSidenavModule} from '@angular/material/sidenav'
 import {MatButtonModule} from '@angular/material/button'
 import {MatDividerModule} from '@angular/material/divider'
+import {MatTreeModule} from '@angular/material/tree'
+import { HttpClientModule } from '@angular/common/http';
+import { MatListModule } from '@angular/material/list'
+import { MatTabsModule } from '@angular/material/tabs';
+import { CommunityComponent } from './community/community.component';
+import { HomepageComponent } from './homepage/homepage.component'
  @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
-    LoginComponent
+    CommonMenuComponent,
+    LoginComponent,
+    CommunityComponent,
+    HomepageComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -36,8 +45,25 @@ import {MatDividerModule} from '@angular/material/divider'
     MatIconModule,
     MatSidenavModule,
     MatButtonModule,
-    MatDividerModule
+    MatDividerModule,
+    MatTreeModule,
+    HttpClientModule,
+    MatListModule,
+    MatTabsModule
   ],
+  exports: [ MatNativeDateModule,
+    MatInputModule,
+    MatSliderModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatTreeModule,
+    HttpClientModule,
+    MatListModule,
+    MatTabsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
