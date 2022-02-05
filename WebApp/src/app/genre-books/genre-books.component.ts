bd468836b0034ed5682f42b9487e59859c5c39f9
Manish Kumar Gangula <41356950+Manish0490@users.noreply.github.com>
2022-02-04 21:50:36 -0500

    Dynamic Genre Description and Images

--- WebApp/src/app/genre-books/genre-books.component.ts
+++ WebApp/src/app/genre-books/genre-books.component.ts
@@ -1,38 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../services/api.service';
 import { GlobalService } from '../services/global.service';
-
+import { ActivatedRoute } from '@angular/router';
+import { GenreDetailsModel } from '../models/general-models';
 @Component({
   selector: 'app-genre-books',
   templateUrl: './genre-books.component.html',
   styleUrls: ['./genre-books.component.scss']
 })
-export class GenreBooksComponent implements OnInit {
 
-  constructor() { }
+export class GenreBooksComponent implements OnInit {
+  books:GenreDetailsModel[]=[];
+  constructor(private api:AppService, private route:ActivatedRoute, public global: GlobalService) { }
   //books = ["The Shining", "White is Witching", "The only good Indians", "The Outsider", "Reprieve", "Interviewing the Vampire", "Book7", "book8", "book9", "book10"];
-  books = [
-    { name: "book1", img: "book1.jpg", desc: "hii" },
-    { name: "book2", img: "book2.jpg", desc: "hi2" },
-    { name: "book3", img: "book3.jpg", desc: "hi3" },
-    { name: "book4", img: "book4.jpg", desc: "hi4" },
-    { name: "book5", img: "book5.jpg", desc: "hi5" },
-    { name: "book6", img: "book6.jpg", desc: "hi6" },
-    { name: "book7", img: "book7.jpg", desc: "hi7" },
-    { name: "book8", img: "book8.jpg", desc: "hi8" },
-    { name: "book9", img: "book9.jpg", desc: "hi9" },
-    { name: "book10", img: "book10.jpg", desc: "hi10" },
-    { name: "book11", img: "book11.jpg", desc: "hi11" },
-    { name: "book12", img: "book12.jpg", desc: "hi12" }
-  ]
   description = "";
   selectedbook = "";
+  bookId = this.route.snapshot.params.id;
 
   ngOnInit(): void {
+    console.log(this.global.genreData);
+    this.api.getGenreDetails(String(this.bookId)).subscribe((data:GenreDetailsModel[])=>{
+      this.books=data;
+    })
   }
   showDescription(name: string) {
     this.selectedbook = name;
-    this.description = this.books.filter(x => x.name == name)[0].desc;
+    // this.description = this.books.filter(x => x.name == name)[0].desc;
 
   }
 }
