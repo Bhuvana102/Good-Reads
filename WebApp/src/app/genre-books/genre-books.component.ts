4718193ec4b4d8e8a7cc09857cea8722c1b1b476
vaishaliunnikrishnan <vaishaliunnikrishnan@gmail.com>
2022-02-05 07:39:05 +0530

    Final horror page

--- WebApp/src/app/genre-books/genre-books.component.ts
+++ WebApp/src/app/genre-books/genre-books.component.ts
@@ -8,17 +8,35 @@ import { GlobalService } from '../services/global.service';
   styleUrls: ['./genre-books.component.scss']
 })
 export class GenreBooksComponent implements OnInit {
-  constructor(private api:AppService, public global: GlobalService) { 
-    console.log(this.global.genreData);
-  }
-  genreId: string = '5'
-  genreDetails:Array<any>=[]
-  ngOnInit(): void {
-    console.log('entered genre');
-    this.api.getGenreDetails(this.genreId).subscribe((data)=>{
-      this.genreDetails = data;
-      console.log(this.genreDetails);
-    })
-  }
 
+  constructor() { }
+  //books = ["The Shining", "White is Witching", "The only good Indians", "The Outsider", "Reprieve", "Interviewing the Vampire", "Book7", "book8", "book9", "book10"];
+  books = [
+    { name: "book1", img: "book1.jpg", desc: "hii" },
+    { name: "book2", img: "book2.jpg", desc: "hi2" },
+    { name: "book3", img: "book3.jpg", desc: "hi3" },
+    { name: "book4", img: "book4.jpg", desc: "hi4" },
+    { name: "book5", img: "book5.jpg", desc: "hi5" },
+    { name: "book6", img: "book6.jpg", desc: "hi6" },
+    { name: "book7", img: "book7.jpg", desc: "hi7" },
+    { name: "book8", img: "book8.jpg", desc: "hi8" },
+    { name: "book9", img: "book9.jpg", desc: "hi9" },
+    { name: "book10", img: "book10.jpg", desc: "hi10" },
+    { name: "book11", img: "book11.jpg", desc: "hi11" },
+    { name: "book12", img: "book12.jpg", desc: "hi12" }
+  ]
+  description = "";
+  selectedbook = "";
+
+  ngOnInit(): void {
+  }
+  showDescription(name: string) {
+    this.selectedbook = name;
+    this.description = this.books.filter(x => x.name == name)[0].desc;
+
+  }
 }
+
+
+
+
