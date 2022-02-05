be3251fdc65deb871af35906711975bbb45cf210
Manish Kumar Gangula <41356950+Manish0490@users.noreply.github.com>
2022-02-04 19:59:54 -0500

    book description details page

--- WebApp/src/app/common-menu/common-menu.component.ts
+++ WebApp/src/app/common-menu/common-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { GenreModel } from '../models/general-models';
 import { AppService } from '../services/api.service';
 import { GlobalService } from '../services/global.service';
 
@@ -12,10 +13,10 @@ export class CommonMenuComponent implements OnInit {
 
   constructor(private api:AppService, private router:Router, 
               public global:GlobalService) { }
-  public genres:Array<any> = [];
+  public genres:GenreModel[]=[];
   opened:boolean = false;
   ngOnInit(): void {
-    this.api.getGenres().subscribe((data)=>{
+    this.api.getGenres().subscribe((data:GenreModel[])=>{
       console.log(data);
       this.genres = data;
     });
@@ -24,19 +25,25 @@ export class CommonMenuComponent implements OnInit {
   goToHome(){
     this.opened = !this.opened;
     this.global.activePage='home';
+    this.router.navigate(['/home']);
   }
   goToGenre(genre:string){
     this.opened = !this.opened;
     this.global.activePage='genre';
+    console.log(this.genres);
+    this.global.genreData = this.genres.filter(genreItem=>genreItem.Id===genre)[0];
+    console.log(this.global.genreData);
     this.router.navigate(['/genre']);
   }
   goToMyBooks(){
     this.opened = !this.opened;
     this.global.activePage='mybooks';
+    this.router.navigate(['/myBooks']);
   }
   goToCommunity(){
     this.opened = !this.opened;
     this.global.activePage='community';
+    this.router.navigate(['/community']);
   }
 
 }
---
+++ WebApp/src/app/models/general-models.ts
@@ -0,0 +1,5 @@
+export interface GenreModel {
+    Id: string;
+    Name: string;
+    Description: string;
+  }
