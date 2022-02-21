5655f2bb8d18d33d3e03ce80b35f9129a00214c8
Bhuvana102 <bhuvanav102@gmail.com>
2022-02-20 22:52:22 -0500

    Modifying the Books table to Store the reviews and Review count

--- backend/model/insertBookFunc.go
+++ backend/model/insertBookFunc.go
@@ -8,6 +8,6 @@ import (
 func InsertBookFunc(book views.Book) error {
 	book_insert := views.Book{Name: book.Name, Description: book.Description, AuthorName: book.AuthorName, GenreId: book.GenreId, PublisherName: book.PublisherName, ImageName: book.ImageName}
 	DB.Select("name", "description", "author_name", "genre_id", "publisher_name", "image_name").Create(&book_insert)
-	fmt.Println("Data inserted to DB with ID : ", book_insert.ID)
+	fmt.Println("Data inserted to DB with ID : ", book_insert.BookID)
 	return nil
 }
--- backend/views/structs.go
+++ backend/views/structs.go
@@ -14,13 +14,15 @@ type Genres struct {
 }
 
 type Book struct {
-	ID            uint   `gorm:"primaryKey"`
+	BookID        uint   `gorm:"primaryKey"`
 	Name          string `JSON:"name"`
 	Description   string `JSON:"description"`
 	AuthorName    string `JSON:"authorname"`
 	GenreId       int    `JSON:"genreid"`
 	PublisherName string `JSON:"publishername"`
 	ImageName     string `JSON:"imagename"`
+	TotRatingCnt  int    `JSON:"count"`
+	TotReviewsCnt int    `JSON:"count"`
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
 }
