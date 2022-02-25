3cc566b179afa4382b7c50d05f65166ae66dc170
Manish Kumar Gangula <41356950+Manish0490@users.noreply.github.com>
2022-02-25 18:51:42 -0500

    Login Page Modification

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -25,6 +25,9 @@ import { HomepageComponent } from './homepage/homepage.component'
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { GenreBooksComponent } from './genre-books/genre-books.component';
 import { MyBooksComponent } from './my-books/my-books.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatCardModule} from '@angular/material/card';
+
  @NgModule({
   declarations: [
     AppComponent,
@@ -56,6 +59,8 @@ import { MyBooksComponent } from './my-books/my-books.component';
     MatListModule,
     MatTabsModule,
     CarouselModule,
+    MatSnackBarModule,
+    MatCardModule
   ],
   exports: [ MatNativeDateModule,
     MatInputModule,
@@ -69,7 +74,8 @@ import { MyBooksComponent } from './my-books/my-books.component';
     MatTreeModule,
     HttpClientModule,
     MatListModule,
-    MatTabsModule,],
+    MatTabsModule,
+    MatSnackBarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
