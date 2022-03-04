6f4ed2101b7ce4b25b7bbc342a5ed63a375bd1e1
Manish Kumar Gangula <41356950+Manish0490@users.noreply.github.com>
2022-03-04 12:28:48 -0500

    Cypress Testing Login Feature

--- WebApp/cypress/integration/login.specs.ts
+++ WebApp/cypress/integration/login.specs.ts
@@ -1,8 +1,29 @@
 import * as cypress from "cypress";
-
-
 describe('Login Feature',()=>{
-    it("Login Input",()=>{
-        cy.visit('/login')
+    it("Login Input without any credentials",()=>{
+        cy.visit('/login');
+        cy.get("#loginBtn").click(); 
+    })
+    it("Login Input with just Login Id and no Password",()=>{
+        cy.visit('/login');
+        cy.get("#loginId").type("Test_User_001");
+        cy.get("#loginBtn").click(); 
+    })
+    it("Login Input with just password and no login Id",()=>{
+        cy.visit('/login');
+        cy.get("#loginPassword").type("Test_Password_001");
+        cy.get("#loginBtn").click(); 
+    })
+    it("Login Input with incorrect credentials",()=>{
+        cy.visit('/login');
+        cy.get("#loginId").type("Test_User_001");
+        cy.get("#loginPassword").type("Test_Password_001");
+        cy.get("#loginBtn").click(); 
+    })
+    it("Login Input with valid credentials",()=>{
+        cy.visit('/login');
+        cy.get("#loginId").type("admin");
+        cy.get("#loginPassword").type("admin");
+        cy.get("#loginBtn").click(); 
     })
 });
