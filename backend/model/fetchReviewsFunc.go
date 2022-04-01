ab80287cc1b97a1fb073551f73c8edce6f2a5a99
Bhuvana102 <bhuvanav102@gmail.com>
2022-04-01 18:03:23 -0400

    Retrieve Reviews for a given bookID

--- backend/model/fetchReviewsFunc.go
+++ backend/model/fetchReviewsFunc.go
@@ -1,18 +1,21 @@
 package model
 
 import (
+	"fmt"
 	"good-reads/views"
 	"strconv"
 )
 
-func FetchReviewsFunc(id string) []views.Review {
+func FetchReviewsFunc(id string, bookID string) []views.Review {
 	var reviews []views.Review
 	rid, _ := strconv.Atoi(id)
 	var ids1 = id
+	println(ids1)
 	for i := 1; i < 10; i++ {
 		temp := int(rid) + i
 		ids1 = ids1 + "," + strconv.Itoa(temp)
 	}
-	DB.Where("Review_Num IN (" + ids1 + ")").Find(&reviews)
+	fmt.Println("Review_Num IN (" + ids1 + ") AND Book_ID = " + bookID)
+	DB.Where("Review_Num IN (" + ids1 + ") AND Book_ID = " + bookID).Find(&reviews)
 	return reviews
 }
