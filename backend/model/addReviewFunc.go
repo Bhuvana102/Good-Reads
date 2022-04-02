a27afdbc4abb42dec373153593312c141db597c9
Bhuvana102 <bhuvanav102@gmail.com>
2022-04-01 23:51:13 -0400

    Modifications to add Reviews

--- backend/model/addReviewFunc.go
+++ backend/model/addReviewFunc.go
@@ -7,11 +7,13 @@ import (
 	"gorm.io/gorm"
 )
 
-func AddReviewFunc(review views.Review) error {
+func AddReviewFunc(review views.Review) (views.Review, error) {
 	review_insert := views.Review{Book_ID: review.Book_ID, User_ID: review.User_ID, Rating: review.Rating, Review: review.Review}
 	DB.Select("Book_ID", "User_ID", "Rating", "Review").Create(&review_insert)
 	fmt.Println("Data inserted to DB with ID : ", review_insert.Review_Num)
 	DB.Model(views.Book{}).Where("ID = ?", review.Book_ID).UpdateColumn("no_of_ratings", gorm.Expr("no_of_ratings + ?", 1))
 	DB.Model(views.Book{}).Where("ID = ?", review.Book_ID).UpdateColumn("no_of_reviews", gorm.Expr("no_of_reviews + ?", 1))
-	return nil
+
+	DB.First(&review, "Review_Num = ?", review_insert.Review_Num)
+	return review, nil
 }
