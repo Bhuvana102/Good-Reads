1e72c0ef9a44fa3ea2f755671cc9f114db24d8af
Bhuvana102 <bhuvanav102@gmail.com>
2022-04-20 17:37:04 -0400

    controller to Fetch Top Rated Books For a given Genre ID

---
+++ backend/controller/fetchTopBooksForGenre.go
@@ -0,0 +1,22 @@
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"good-reads/model"
+	"net/http"
+)
+
+func FetchTopBooksForGenre() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if r.Method == http.MethodGet {
+			fmt.Println("Fetching top Books for reqested Genre ID:", r.URL.Query().Get("id"))
+			gId := r.URL.Query().Get("id")
+			data := model.FetchBookRatingsFunc(gId)
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(data)
+			// fmt.Println(data)
+		}
+	}
+}
