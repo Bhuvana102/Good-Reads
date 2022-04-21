f58d21ce24dca53be5c79b55eb5f3b12db31fc39
Manish Kumar Gangula <41356950+Manish0490@users.noreply.github.com>
2022-04-21 14:36:53 -0400

    Sprint4-TestCommit

--- WebApp/src/app/login/login.component.ts
+++ WebApp/src/app/login/login.component.ts
@@ -20,11 +20,46 @@ export class LoginComponent implements OnInit {
   remail:string='';
   rpassword:string='';
   rcpassword:string='';
-
+  emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  passwordRegex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
   constructor(private router: Router, private snackBar:MatSnackBar) { 
   }
 
   ngOnInit(): void {
+  }
+
+  onEmailFocusOut(){
+    if(this.remail===''){
+      this.snackBar.open("Email Cannot be Empty",'',{duration:1000});
+    }
+    else if(!this.emailRegex.test(this.remail)){
+      this.snackBar.open("Enter Valid Email",'',{duration:1000})
+    }
+    else{
+      return true
+    }
+  }
+  onPasswordFocusOut(){
+    if(this.rpassword===''){
+      this.snackBar.open("Password Cannot be Blank",'',{duration:1000});
+    }
+    else if(!this.passwordRegex.test(this.rpassword)){
+      this.snackBar.open("Enter Valid Password",'',{duration:3000});
+    }
+    else{
+      return true
+    }
+  }
+  onConfPasswordFocusOut(){
+    if(this.rcpassword===''){
+      this.snackBar.open("Confirm Password Cannot be Empty",'',{duration:1000});
+    }
+    else if(!this.passwordRegex.test(this.rcpassword)){
+      this.snackBar.open("Enter Valid Confirm Password",'',{duration:1000})
+    }
+    else{
+      return true
+    }
   }
 
   doLogin(){
@@ -32,7 +67,10 @@ export class LoginComponent implements OnInit {
   }
 
   register() {
-
+    if(!this.onEmailFocusOut()) return
+    else if(!this.onPasswordFocusOut()) return
+    else if(!this.onConfPasswordFocusOut()) return
+    else console.log('test')
   }
   login() {
     if(this.email=="admin" && this.password=="admin"){
@@ -49,7 +87,7 @@ export class LoginComponent implements OnInit {
         this.snackBar.open('Please enter a password','',{duration:2000})
       }
       else{
-      this.snackBar.open('Invalid Login Id or Password','',{duration:2000})
+        this.snackBar.open('Invalid Login Id or Password','',{duration:2000})
       }
     }
   }
