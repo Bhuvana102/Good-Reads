03366ffa0a6daaf46e2544b24c2ff9d531efcd8a
Srikar Chowdary Kantamani <srikar.kantamani@gmail.com>
2022-04-21 21:39:24 -0400

    fixing CORS issue login authentication api

--- backend/controller/authenticateUserHandler.go
+++ backend/controller/authenticateUserHandler.go
@@ -9,12 +9,14 @@ import (
 
 func AuthenticateUserHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		if r.Method == http.MethodPost {
 			data := views.User_Auth{}
 			json.NewDecoder(r.Body).Decode(&data)
 			user_auth := model.AuthenticateUserFunc(data)
-			w.WriteHeader(http.StatusFound)
+			w.WriteHeader(http.StatusOK)
 			user_auth.Password = ""
 			json.NewEncoder(w).Encode(user_auth)
 		}
